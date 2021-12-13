fdcb088064b2ee14776629eb1b838e865e693596
Lucas Gabriel <66486119+lucasgabriel07@users.noreply.github.com>
2021-12-13 18:58:15 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+class Estado:
+
+    def __init__(self, final=False):
+        self.transicoes = {}  # Dicionário com as possíveis transicoes do estado. key = letra; value = próximo estado
+        self.final = final  # Igual a True se o estado for final
+
+    def adicionar_transicao(self, letra, transicao):
+        self.transicoes[letra] = transicao
+
+    def next(self, letra):
+        if letra in self.transicoes:
+            return self.transicoes[letra]
+        else:
+            return None
+
+
+def validar_palavra(estado_inicial, palavra):
+    estado_atual = estado_inicial
+
+    for letra in palavra:
+        estado_atual = estado_atual.next(letra)
+        if estado_atual is None:
+            return False
+    """ 
+    Se a função chegou até aqui, então a palavra foi totalmente processada.
+    A função então verifica se o estado atual é um estado final. Se for um
+    estado final, então a palavra é aceita.
+    """
+    return estado_atual.final
+
+
+if __name__ == '__main__':
+
+    q0 = Estado()
+    q1 = Estado()
+    q2 = Estado(final=True)
+
+    q0.adicionar_transicao('a', q0)
+    q0.adicionar_transicao('b', q1)
+    q1.adicionar_transicao('b', q2)
+    q2.adicionar_transicao('c', q1)
+
+    while True:
+
+        string = input('Digite uma palavra: ')
+
+        if validar_palavra(q0, string):
+            print('aceita\n')
+        else:
+            print('rejeita\n')
